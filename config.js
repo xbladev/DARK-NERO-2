4007ccc6e4aa87690905358ccfe69b14ce38b68a
xbladev <blademyx@gmail.com>
2025-07-26 18:55:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'GENUX-08a1ce',
+    SESSION_ID: process.env.SESSION_ID || 'GENUX-fab58b',
     PREFIX: process.env.PREFIX || '.',
     OWNER_NUMBER: process.env.OWNER_NUMBER || ['94764497078'],
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://exekieligop:ul8XnJl05zUDHrEu@sandaru.tc3wtqw.mongodb.net',
